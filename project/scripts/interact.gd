6472b336d9d9a35c6eab92a6d04d107006fc84b0
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-02-12 22:12:30 +0100

    Finished the interact system

--- project/scripts/interact.gd
+++ project/scripts/interact.gd
@@ -1,18 +1,16 @@
 extends Area3D
 class_name InteractZone
-@export_dir var message:String
+@export_file("*.tscn") var message:String
 var info
 
 func _ready() -> void:
 	var hitbox= CollisionShape3D.new()
-	hitbox.shape=BoxShape3D
-	#hitbox.shape.extends=self.shape.extends
-	
+	hitbox.shape=BoxShape3D.new()
 	add_child(hitbox)
 
 func interact():
-	info=load(message).instantiate()
-	add_sibling(info)
+	info= load(message).instantiate()
+	add_child(info)
 
 func remove():
 	info.queue_free()
--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -20,6 +20,11 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
-	print("Area entered")
 	if (area is InteractZone):
 		area.interact()
+		print("interacted")
+
+
+func _on_area_3d_area_exited(area: Area3D) -> void:
+	if (area is InteractZone):
+		area.remove()
