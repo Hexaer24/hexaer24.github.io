d369a222f8cb7896f25df44981a6afc87a41894a
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-02-19 14:43:19 +0100

    Grass tutorial

---
+++ project/test/camera_spinner.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+var angle: float = 0.0
+@export var angular_speed: float = 0.1
+
+func _process(delta):
+	angle += delta * angular_speed
+	transform.basis = Basis(Quaternion(Vector3.UP, angle))
