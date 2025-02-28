9e9fb4857863d70d038055ad64eb1cbea0404119
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-02-28 10:39:06 +0100

    Started making move_to in camera

---
+++ project/scenes/camera.gd
@@ -0,0 +1,16 @@
+extends Camera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func move_to(there:Vector3, rot:Vector3):
+	position=there
+	rotation_degrees=rot
+	
