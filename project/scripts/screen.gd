9a8a51d638efa573515415e785e248217cfb407a
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-03-05 12:55:43 +0100

    Somewhat working test UI

---
+++ project/scripts/screen.gd
@@ -0,0 +1,8 @@
+extends MeshInstance3D
+
+@onready var viewport := $SubViewport
+@onready var desktop :=$SubViewport/os/Desktop
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
