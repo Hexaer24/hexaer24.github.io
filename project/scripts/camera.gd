056299a6ae098741ab337e29ac7beaaa98f96050
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-04-08 10:04:26 +0200

    Nevermind it's not even real

--- project/scripts/camera.gd
+++ project/scripts/camera.gd
@@ -1,15 +1,36 @@
 extends Marker3D
 var camera:Camera3D
+var time=0
 
 func _ready() -> void:
 	camera=get_node("Camera3D")
+	
 
-func move_camera_to(curve_point:Vector3,there:Vector3=position, rot:Vector3=rotation_degrees ):
-	camera.global_position=there
-	camera.global_rotation_degrees=rot
+func move_camera_to(curve_point:Vector3,there=self, rot:Vector3=global_rotation_degrees):
+	var tween=get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.tween_property(camera,"global_rotation_degrees",rot,1)
+	if there is Node3D:
+		move_to_node(there,curve_point)
+	elif there is Vector3:
+		move_to_vector(there,curve_point)
+	time=0
+
+func move_to_node(there,curve_point):
+	while (time<1):
+		camera.global_position=adaptive_bezier(time,there.global_position,curve_point)
+		print(there.global_positiond)
+		time+=get_process_delta_time()
+		await get_tree().process_frame
+	
+func move_to_vector(there,curve_point):
+	while (time<1):
+		camera.global_position=adaptive_bezier(time,there,curve_point)
+		time+=get_process_delta_time()
+		await get_tree().process_frame
 
 func adaptive_bezier(t,there:Vector3,curve_point:Vector3):
-	var q0=self.position.lerp(curve_point,t)
+	var q0=camera.global_position.lerp(curve_point,t)
 	var q1=curve_point.lerp(there,t)
 	var r=q0.lerp(q1,t)
 	return r
--- project/scripts/interact.gd
+++ project/scripts/interact.gd
@@ -4,9 +4,11 @@ class_name InteractZone extends Area3D
 
 var info
 var not_camera
+var thread:Thread
 
 func _ready() -> void:
 	not_camera=get_node("../player/Marker3D")
+	thread = Thread.new()
 	
 	connect("body_entered", _on_body_entered)
 	connect("body_exited", _on_body_exited)
@@ -15,15 +17,14 @@ func _ready() -> void:
 	add_child(hitbox)
 
 func interact():
-	not_camera.move_camera_to( Vector3(0,1,0),Vector3(-1.1,1.3,-1.1,), Vector3(0,45,0))
+	not_camera.move_camera_to(Vector3(0,1,0),Vector3(-1.1,1.3,-1.1), Vector3(0,45,0))
 
 
 func _on_body_entered(body) -> void:
 	if (body is CharacterBody3D):
-		print(not_camera.position,not_camera.get_child(0).position)
 		interact()
 
 
 func _on_body_exited(body) -> void:
-	not_camera.move_camera_to(Vector3(0,1,0))
-	print(not_camera.position,not_camera.get_child(0).position)
+	if (body is CharacterBody3D):
+		not_camera.move_camera_to(Vector3(0,1,0))
