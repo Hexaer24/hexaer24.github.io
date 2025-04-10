92eb7800b979015cee0ad1f11a9cdb382c32b8c8
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-04-10 11:51:59 +0200

    Making a camera manager is hard

--- project/scripts/camera.gd
+++ project/scripts/camera.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	camera=get_node("Camera3D")
 
 func move_camera_to(curve_point:Vector3,there=self):
-	while (!terminate and time<1):
+	while (time<1):
 		camera.global_position=adaptive_bezier(time,there.global_position,curve_point)
 		#camera.global_rotation=adaptive_bezier(time,there.global_rotation_degrees,curve_point)
 		time+=get_process_delta_time()
@@ -20,3 +20,8 @@ func adaptive_bezier(t,there:Vector3,curve_point:Vector3):
 	var q1=curve_point.lerp(there,t)
 	var r=q0.lerp(q1,t)
 	return r
+
+#Gonna have to manage the thingy here, already kinda made it Awaria3D
+# Plan:
+# - Make add target, with current objective variable. It'll be more like a request like add_pull_source.
+# - While still an objective, run the loop. This is to not rely on physics_process
--- project/scripts/interact.gd
+++ project/scripts/interact.gd
@@ -9,7 +9,6 @@ var thread:Thread
 func _ready() -> void:
 	marker=get_node(marker_path)
 	not_camera=get_node("../player/Marker3D")
-	thread = Thread.new()
 	connect("body_entered", _on_body_entered)
 	connect("body_exited", _on_body_exited)
 	var hitbox= CollisionShape3D.new()
@@ -20,11 +19,9 @@ func _ready() -> void:
 
 func _on_body_entered(body) -> void:
 	if (body is CharacterBody3D):
-		print("Entered")
 		not_camera.move_camera_to(Vector3(0,1,0),marker)
 
 
 func _on_body_exited(body) -> void:
 	if (body is CharacterBody3D):
-		print("Exited")
 		not_camera.move_camera_to(Vector3(0,1,0))
