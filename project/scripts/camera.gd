954b91272ec55784a87d64b3224b4edfcfb0afeb
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-04-11 14:06:07 +0200

    Zoom in works, only lacks rotation

--- project/scripts/camera.gd
+++ project/scripts/camera.gd
@@ -1,20 +1,26 @@
 extends Marker3D
 var camera:Camera3D
 var time=0
-var current_task=Thread.new()
-var terminate=false
+var current_objective:Marker3D
+var running= false
 
 func _ready() -> void:
 	camera=get_node("Camera3D")
 
 func move_camera_to(curve_point:Vector3,there=self):
-	while (time<1):
-		camera.global_position=adaptive_bezier(time,there.global_position,curve_point)
-		#camera.global_rotation=adaptive_bezier(time,there.global_rotation_degrees,curve_point)
-		time+=get_process_delta_time()
-		await get_tree().process_frame
-	time=0
-
+	current_objective=there
+	if (!running):
+		running = true
+		while (time<1):
+			camera.global_position=adaptive_bezier(time,current_objective.global_position,curve_point)
+			#camera.global_rotation=adaptive_bezier(time,there.global_rotation_degrees,curve_point)
+			time+=get_process_delta_time()
+			await get_tree().process_frame
+		time=0
+		running =false
+	else:
+		time=1-time
+		
 func adaptive_bezier(t,there:Vector3,curve_point:Vector3):
 	var q0=camera.global_position.lerp(curve_point,t)
 	var q1=curve_point.lerp(there,t)
