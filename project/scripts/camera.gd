610aef2bb5fce5331b83b23cde11415418620f1d
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-04-15 16:37:01 +0200

    Idek

--- project/scripts/camera.gd
+++ project/scripts/camera.gd
@@ -12,14 +12,13 @@ func move_camera_to(curve_point:Vector3,there=self):
 	if (!running):
 		running = true
 		while (time<1):
-			camera.global_position=adaptive_bezier(time,current_objective.global_position,curve_point)
-			#camera.global_rotation=adaptive_bezier(time,there.global_rotation_degrees,curve_point)
+			camera.global_position=adaptive_bezier(ease_in_out(time),current_objective.global_position,curve_point)
 			time+=get_process_delta_time()
 			await get_tree().process_frame
 		time=0
 		running =false
 	else:
-		time=1-time
+		time=0
 		
 func adaptive_bezier(t,there:Vector3,curve_point:Vector3):
 	var q0=camera.global_position.lerp(curve_point,t)
@@ -27,7 +26,16 @@ func adaptive_bezier(t,there:Vector3,curve_point:Vector3):
 	var r=q0.lerp(q1,t)
 	return r
 
-#Gonna have to manage the thingy here, already kinda made it Awaria3D
-# Plan:
-# - Make add target, with current objective variable. It'll be more like a request like add_pull_source.
-# - While still an objective, run the loop. This is to not rely on physics_process
+func ease_in_out(t: float) -> float:
+	return t * t *t*t* (3.0 - 2.0 * t)
+
+"""func get_curve_point_with_steering(from: Vector3, to: Vector3) -> Vector3:
+	var desired = (to - from).normalized()
+	var space = get_world_3d().direct_space_state
+	var result = space.intersect_ray(from, to, [camera])
+
+	if result:
+		var avoid_dir = (result.normal + Vector3.UP).normalized()
+		return from.lerp(to, 0.5) + avoid_dir * 3.0
+	else:
+		return from.lerp(to, 0.5) + Vector3.UP * 2"""
