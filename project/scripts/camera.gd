7d87cb5e1fd3a678e23aa470347596475d46f565
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-04-15 18:24:46 +0200

    Kinda working rotation, need to fix mid-swing turning

--- project/scripts/camera.gd
+++ project/scripts/camera.gd
@@ -7,18 +7,47 @@ var running= false
 func _ready() -> void:
 	camera=get_node("Camera3D")
 
-func move_camera_to(curve_point:Vector3,there=self):
+## Funni lambda function to avoid repeated if in while loop. Not necessary but cool
+## Rip old ternary operator: a if boolean else b
+func move_camera_to(curve_point:Vector3,there=self,reverse=false, look_object=null):
 	current_objective=there
 	if (!running):
 		running = true
+		if look_object==null:
+			look_object=there
+		var get_look_target
+		if reverse:
+			get_look_target = func(pos):
+				var direction = (look_object.global_position - pos).normalized()
+				return pos - direction
+		else:
+			get_look_target = func(pos):
+				return look_object.global_position
 		while (time<1):
+			"""camera.look_at(get_look_target.call(camera.position))
 			camera.global_position=adaptive_bezier(ease_in_out(time),current_objective.global_position,curve_point)
+			"""
+			var pos = adaptive_bezier(ease_in_out(time), current_objective.global_position, curve_point)
+			camera.global_position = pos
+
+			# Target look-at position
+			var target_look_pos = get_look_target.call(pos)
+			
+			# Direction the camera should look towards
+			var target_direction = (target_look_pos - pos).normalized()
+			var current_basis = camera.global_transform.basis
+			var target_basis = Basis().looking_at(target_direction, Vector3.UP)
+			
+			# Interpolate between current and target rotation using slerp
+			var t = clamp(time * 2.0, 0.0, 1.0)  # speed up rotation a bit
+			camera.global_transform.basis = current_basis.slerp(target_basis, t)
 			time+=get_process_delta_time()
 			await get_tree().process_frame
 		time=0
 		running =false
 	else:
 		time=0
+		
 		
 func adaptive_bezier(t,there:Vector3,curve_point:Vector3):
 	var q0=camera.global_position.lerp(curve_point,t)
--- project/scripts/interact.gd
+++ project/scripts/interact.gd
@@ -1,27 +1,32 @@
 class_name InteractZone extends Area3D
 @export_node_path("Marker3D") var marker_path
+@export_node_path() var look_object_path
 var marker
-
+var look_object
 var info
 var not_camera:Node3D
-var thread:Thread
+var size=Vector3(0.1,0.1,0.1)
 
 func _ready() -> void:
 	marker=get_node(marker_path)
+	if look_object_path!=null:
+		look_object=get_node(look_object_path)
+	
 	not_camera=get_node("../player/Marker3D")
 	connect("body_entered", _on_body_entered)
 	connect("body_exited", _on_body_exited)
 	var hitbox= CollisionShape3D.new()
 	hitbox.shape=BoxShape3D.new()
+	hitbox.shape.size=size
 	add_child(hitbox)
 	
 
 
 func _on_body_entered(body) -> void:
 	if (body is CharacterBody3D):
-		not_camera.move_camera_to(Vector3(0,1,0),marker)
+		not_camera.move_camera_to(Vector3(0,1,0),marker,false,look_object)
 
 
 func _on_body_exited(body) -> void:
 	if (body is CharacterBody3D):
-		not_camera.move_camera_to(Vector3(0,1,0))
+		not_camera.move_camera_to(Vector3(0,1,0),not_camera,true,)
