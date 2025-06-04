05d74cc4c40b0b9ace39b9e913eaba1fcd51fae4
Hexaer <92447204+Hexaer24@users.noreply.github.com>
2025-06-04 15:22:19 +0200

    Textures finished

--- project/scripts/desktop_button.gd
+++ project/scripts/desktop_button.gd
@@ -3,6 +3,7 @@ class_name DesktopButton extends TextureButton
 @export var opt_title:String
 @onready var os=$"../.."
 var background
+var extension
 
 func _ready() -> void:
 	focus_mode=Control.FOCUS_NONE
@@ -10,16 +11,21 @@ func _ready() -> void:
 	var label=Label.new()
 	add_child(label)
 	var split_path = path.split("/")
+	extension=split_path[-1].split(".")[-1]
 	if split_path[0]!="uid:":
 		label.text=split_path[-1]
-		set_icon(split_path[-1])
+		set_icon(extension)
 	else:
 		label.text=opt_title
-		set_icon(".txt")
+		set_icon("txt")
 	label.position.y=-10
 	
 func set_icon(extension):
-	
+	var assign=func(path):
+		var new_texture:Texture2D=load(path)
+		texture_hover=new_texture
 	match extension:
-		".txt":
-			texture_hover=Texture2D.new()
+		"txt":
+			assign.call("res://assets/notepad-hover.png")
+		_:
+			texture_hover=null
